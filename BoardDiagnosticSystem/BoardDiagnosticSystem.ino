415663a21ac543cfc27da51ecba116fcb2dca843
rinojk <>
2022-02-01 23:07:21 +0200

    Seems to be working with display and communication

--- BoardDiagnosticSystem/BoardDiagnosticSystem.ino
+++ BoardDiagnosticSystem/BoardDiagnosticSystem.ino
@@ -1,6 +1,53 @@
-#include <EasyTransferI2C.h>
-#include <Wire.h>
 #include "KWP.h"
+
+#include <Wire.h>
+#include "i2cSimpleTransfer.h"
+
+#pragma region I2Cmanaging
+#define i2c_slave_address 17
+
+// You can add more variables into the struct, but the default limit for transfer size in the Wire library is 32 bytes
+struct SLAVE_DATA
+{
+    int16_t sensor = 0; // use specific declarations to ensure communication between 16bit and 32bit controllers
+    int16_t connectionStatus = 1;
+    int16_t oilTemp = 89;
+    int16_t coolantTemp = 90;
+    int16_t MAF = 91;
+    int16_t misfireCounter = 92;
+    int16_t batteryVoltage = 93;
+};
+
+struct SLAVE_CONFIG
+{
+    uint8_t val; // use specific declarations to ensure communication between 16bit and 32bit controllers
+};
+
+SLAVE_DATA slave_data;
+SLAVE_CONFIG slave_config;
+void setupI2C()
+{
+    Wire.begin(i2c_slave_address); // i2c Slave address
+    Wire.onRequest(requestEvent);  // when the Master makes a request, run this function
+    Wire.onReceive(receiveEvent);  // when the Master sends us data, run this function
+    slave_config.val = 1;          // This is how much we increment after each request
+}
+void requestEvent()
+{
+    slave_data.oilTemp += 1;
+    slave_data.batteryVoltage += 2;
+    i2cSimpleWrite(slave_data); // Send the Master the sensor data
+    //slave_data.sensor += slave_config.val; // Simulate updated sensor data
+}
+
+void receiveEvent(int payload)
+{
+    if (payload == sizeof(slave_config))
+    {
+        i2cSimpleRead(slave_config); // Receive new data from the Master
+    }
+}
+#pragma endregion
 
 #pragma region Timers
 unsigned long loopTimer = 0;
@@ -40,44 +87,22 @@ int count = 0;
 //
 #pragma endregion
 
-#pragma region WireTransfer vars
-//---Communication between Arduino and Seeeduino---//
-//Library: EasyTransfer (https://github.com/madsci1016/Arduino-EasyTransfer)//
-EasyTransferI2C ET;
-
-//define slave i2c address
-#define I2C_SLAVE_ADDRESS 9
-
-struct SEND_DATA_STRUCTURE
-{
-  bool connectionStatus = false;
-  String oilTemp = "";
-  String coolantTemp = "";
-  String MAF = "";
-  String misfireCounter = "";
-  String batteryVoltage = "";
-};
-
-//give a name to the group of data
-SEND_DATA_STRUCTURE wireData;
-#pragma endregion
-
 void setup()
 {
-  setupWireTransfer();
-  setupDiagnostic();
+    Serial.begin(9600);
+    setupI2C();
+    setupDiagnostic();
 }
 
 void loop()
 {
-  Serial.print("Loop elapsed: ");
-  Serial.println(millis() - loopTimer);
-  loopTimer = millis();
+    Serial.print("Loop elapsed: ");
+    Serial.println(millis() - loopTimer);
+    loopTimer = millis();
 
-  manageWireTransfer();
-  manageDiagnostic();
+    manageDiagnostic();
 
-  //showWireDataToSerial();
+    //showWireDataToSerial();
 }
 
 #pragma region Diagnostic Handlers
@@ -87,160 +112,134 @@ void loop()
 // 2 - Down
 void refreshParams(int type)
 {
-  if (type == 1)
-  {
-    if (currentSensor < nSensors - 1)
-      currentSensor++;
-    else
+    if (type == 1)
     {
-      currentSensor = 0;
-      if (currentGroup < (currentModule->ngroups) - 1)
-        currentGroup++;
-      else
-      {
-        if (currentModule->addr == ADR_Dashboard)
-          currentModule = modules[1];
+        if (currentSensor < nSensors - 1)
+            currentSensor++;
         else
-          currentModule = modules[0];
-        currentGroup = 0;
-        kwp.disconnect();
-      }
+        {
+            currentSensor = 0;
+            if (currentGroup < (currentModule->ngroups) - 1)
+                currentGroup++;
+            else
+            {
+                if (currentModule->addr == ADR_Dashboard)
+                    currentModule = modules[1];
+                else
+                    currentModule = modules[0];
+                currentGroup = 0;
+                kwp.disconnect();
+            }
+        }
     }
-  }
-  else if (type == 2)
-  {
-    if (currentSensor > 0)
-      currentSensor--;
-    else
+    else if (type == 2)
     {
-      currentSensor = nSensors - 1;
-      if (currentGroup > 0)
-        currentGroup--;
-      else
-      {
-        if (currentModule->addr == ADR_Dashboard)
-          currentModule = modules[1];
+        if (currentSensor > 0)
+            currentSensor--;
         else
-          currentModule = modules[0];
-        currentGroup = currentModule->ngroups - 1;
-        kwp.disconnect();
-      }
+        {
+            currentSensor = nSensors - 1;
+            if (currentGroup > 0)
+                currentGroup--;
+            else
+            {
+                if (currentModule->addr == ADR_Dashboard)
+                    currentModule = modules[1];
+                else
+                    currentModule = modules[0];
+                currentGroup = currentModule->ngroups - 1;
+                kwp.disconnect();
+            }
+        }
     }
-  }
 }
 
 void setupDiagnostic()
 {
-  bool result = kwp.connect(currentModule->addr, currentModule->baudrate);
+    bool result = kwp.connect(currentModule->addr, currentModule->baudrate);
 }
 
 void manageDiagnostic()
 {
-  if (millis() - dashboardTimer > dashboardPeriod)
-  {
-    currentModule = modules[1];
-    kwp.disconnect();
-  }
-  if (kwp.isConnected())
-  {
-    if (currentModule->name == "ECU")
+    if (millis() - dashboardTimer > dashboardPeriod)
     {
-      SENSOR resultBlock[maxSensors];
-      kwp.readBlock(currentModule->addr, currentModule->groups[1], maxSensors, resultBlock);
-      wireData.coolantTemp = resultBlock[1].value;
-      kwp.readBlock(currentModule->addr, currentModule->groups[2], maxSensors, resultBlock);
-      wireData.MAF = resultBlock[3].value;
-      kwp.readBlock(currentModule->addr, currentModule->groups[16], maxSensors, resultBlock);
-      wireData.misfireCounter = resultBlock[0].value;
-      kwp.readBlock(currentModule->addr, currentModule->groups[51], maxSensors, resultBlock);
-      wireData.batteryVoltage = resultBlock[3].value;
+        currentModule = modules[1];
+        kwp.disconnect();
     }
-    else if (currentModule->name == "DASHBOARD")
+    if (kwp.isConnected())
     {
-      SENSOR resultBlock[maxSensors];
-      kwp.readBlock(currentModule->addr, currentModule->groups[3], maxSensors, resultBlock);
-      wireData.oilTemp = resultBlock[2].value;
-      dashboardTimer = millis();
-    }
-  }
-  else
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("Starting " + currentModule->name);
-    if (kwp.connect(currentModule->addr, currentModule->baudrate))
-    {
-      Serial.println("Con. OK! Reading...");
-      connRetries = 0;
-      digitalWrite(LED_BUILTIN, HIGH);
+        if (currentModule->name == "ECU")
+        {
+            SENSOR resultBlock[maxSensors];
+            kwp.readBlock(currentModule->addr, currentModule->groups[1], maxSensors, resultBlock);
+            //wireData.coolantTemp = resultBlock[1].value;
+            kwp.readBlock(currentModule->addr, currentModule->groups[2], maxSensors, resultBlock);
+            //wireData.MAF = resultBlock[3].value;
+            kwp.readBlock(currentModule->addr, currentModule->groups[16], maxSensors, resultBlock);
+            //wireData.misfireCounter = resultBlock[0].value;
+            kwp.readBlock(currentModule->addr, currentModule->groups[51], maxSensors, resultBlock);
+            //wireData.batteryVoltage = resultBlock[3].value;
+        }
+        else if (currentModule->name == "DASHBOARD")
+        {
+            SENSOR resultBlock[maxSensors];
+            kwp.readBlock(currentModule->addr, currentModule->groups[3], maxSensors, resultBlock);
+            //wireData.oilTemp = resultBlock[2].value;
+            dashboardTimer = millis();
+        }
     }
     else
-    { // Antiblocking
-      Serial.println("KWP not connected");
-      digitalWrite(LED_BUILTIN, LOW);
-      if (connRetries > MAX_CONNECT_RETRIES)
-      {
-        Serial.println("Connection retry " + String(connRetries) + "... Module addr " + String(currentModule->addr));
-        if (currentModule->addr == ADR_Dashboard)
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Starting " + currentModule->name);
+        if (kwp.connect(currentModule->addr, currentModule->baudrate))
         {
-          dashboardTimer = millis();
-          currentModule = modules[0];
+            Serial.println("Con. OK! Reading...");
+            connRetries = 0;
+            digitalWrite(LED_BUILTIN, HIGH);
         }
         else
-          currentModule = modules[1];
-        currentGroup = 0;
-        currentSensor = 0;
-        nSensors = 0;
-        connRetries = 0;
-      }
-      else
-        connRetries++;
+        { // Antiblocking
+            Serial.println("KWP not connected");
+            digitalWrite(LED_BUILTIN, LOW);
+            if (connRetries > MAX_CONNECT_RETRIES)
+            {
+                Serial.println("Connection retry " + String(connRetries) + "... Module addr " + String(currentModule->addr));
+                if (currentModule->addr == ADR_Dashboard)
+                {
+                    dashboardTimer = millis();
+                    currentModule = modules[0];
+                }
+                else
+                    currentModule = modules[1];
+                currentGroup = 0;
+                currentSensor = 0;
+                nSensors = 0;
+                connRetries = 0;
+            }
+            else
+                connRetries++;
+        }
     }
-  }
-}
-//
-#pragma endregion
-
-#pragma region WireTransfer Handlers
-//Arduino Transfer digital wire
-void setupWireTransfer()
-{
-  Wire.begin();
-  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc.
-  ET.begin(details(wireData), &Wire);
-
-  pinMode(13, OUTPUT);
-
-  randomSeed(analogRead(0));
-}
-
-void manageWireTransfer()
-{
-  wireTransferTimer = millis();
-  while (millis() < wireTransferTimer + wireTransferPeriod)
-  {
-    //send the data
-    ET.sendData(I2C_SLAVE_ADDRESS);
-    Serial.println("Wire Transfer in progress");
-  }
 }
 //
 #pragma endregion
 
 void showWireDataToSerial()
 {
-  Serial.print("Oil temp: ");
-  Serial.print(wireData.oilTemp);
-  Serial.println();
-  Serial.print("Coolant temp: ");
-  Serial.print(wireData.coolantTemp);
-  Serial.println();
-  Serial.print("MAF: ");
-  Serial.print(wireData.MAF);
-  Serial.println();
-  Serial.print("Misfires: ");
-  Serial.print(wireData.misfireCounter);
-  Serial.println();
-  Serial.print("Batt voltage: ");
-  Serial.print(wireData.batteryVoltage);
-  Serial.println();
+    Serial.print("Oil temp: ");
+    Serial.print(slave_data.oilTemp);
+    Serial.println();
+    Serial.print("Coolant temp: ");
+    Serial.print(slave_data.coolantTemp);
+    Serial.println();
+    Serial.print("MAF: ");
+    Serial.print(slave_data.MAF);
+    Serial.println();
+    Serial.print("Misfires: ");
+    Serial.print(slave_data.misfireCounter);
+    Serial.println();
+    Serial.print("Batt voltage: ");
+    Serial.print(slave_data.batteryVoltage);
+    Serial.println();
 }
--- BoardInfoSystem/BoardInfoSystem.ino
+++ BoardInfoSystem/BoardInfoSystem.ino
@@ -1,52 +1,141 @@
-#include <EasyTransferI2C.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSans18pt7b.h>
+#include "i2cSimpleTransfer.h"
 
-struct SEND_DATA_STRUCTURE
+#pragma region Display
+///
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+///
+
+//-----------------------Display Module------------------------------//
+void setupDisplay()
 {
-  bool connectionStatus = false;
-  String oilTemp = "";
-  String coolantTemp = "";
-  String MAF = "";
-  String misfireCounter = "";
-  String batteryVoltage = "";
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("SSD1306 allocation failed");
+    for (;;)
+      ;
+  }
+  //display.setFont(&FreeMono9pt7b);
+  display.setTextSize(1);
+  display.setRotation(2);
+  display.setTextColor(WHITE);
+  display.cp437(true);
+  displayInfo("HELLO WRLD!");
+}
+
+void displayInfo(String text)
+{
+  display.clearDisplay();
+
+  display.setFont();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(text);
+  display.display();
+}
+
+void displayInfo(String text1, String text2, String text3, String mainText, bool warning)
+{
+  display.clearDisplay();
+
+  display.setFont();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println(text1);
+  display.println(text2);
+
+  display.setTextSize(2);
+  display.println(text3);
+
+  display.setFont(&FreeSans18pt7b);
+  display.setTextSize(1);
+  int shift = 24;
+  display.setCursor(69, shift);
+  display.println(mainText);
+
+  if (warning)
+  {
+    display.setFont();
+    display.setTextSize(4);
+    display.setCursor(54, 0);
+    display.print("!");
+  }
+  display.display();
+}
+#pragma endregion
+
+#pragma region I2C Master managing
+#define i2c_sensor_slave 17
+
+// You can add more variables into the struct, but the default limit for transfer size in the Wire library is 32 bytes
+struct SLAVE_DATA
+{
+  int16_t sensor = 0; // use specific declarations to ensure communication between 16bit and 32bit controllers
+  int16_t connectionStatus = 0;
+  int16_t oilTemp = 0;
+  int16_t coolantTemp = 0;
+  int16_t MAF = 0;
+  int16_t misfireCounter = 0;
+  int16_t batteryVoltage = 0;
 };
 
-#pragma region WireTransfer vars
-EasyTransferI2C ET; 
-
-//give a name to the group of data
-SEND_DATA_STRUCTURE wireData;
-
-//define slave i2c address
-#define I2C_SLAVE_ADDRESS 9
-#pragma endregion
-
-void setup() {
-  setupWireTransfer();
-}
-
-void loop() {
-  manageWireTransfer();
-}
-
-#pragma region WireTransfer Handlers
-//Arduino Transfer digital wire
-void setupWireTransfer()
+struct SLAVE_CONFIG
 {
-  Wire.begin(I2C_SLAVE_ADDRESS);
-  ET.begin(details(wireData), &Wire);
-  Wire.onReceive(receive);
-  pinMode(13, OUTPUT);
-}
+  uint8_t val; // use specific declarations to ensure communication between 16bit and 32bit controllers
+};
 
-void manageWireTransfer()
+SLAVE_DATA slave_data;
+SLAVE_CONFIG slave_config;
+
+void manageWire()
 {
-  if(ET.receiveData()){
-    
+  Wire.requestFrom(i2c_sensor_slave, sizeof(slave_data)); // request data from the Slave device the size of our struct
+
+  if (Wire.available() == sizeof(slave_data))
+  {
+    i2cSimpleRead(slave_data);
   }
+  showData();
+  displayInfo(String(String(millis()/1000) + " " + String(slave_data.oilTemp)), String(slave_data.coolantTemp), String(slave_data.misfireCounter), String(slave_data.batteryVoltage), false);
+
+  slave_config.val = 100;
+  Wire.beginTransmission(i2c_sensor_slave);
+  i2cSimpleWrite(slave_config);
+  Wire.endTransmission();
 }
 
-void receive(int numBytes){
-}
-//
 #pragma endregion
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+  setupDisplay();
+  Serial.begin(9600); // start serial for output
+}
+
+void loop()
+{
+  manageWire();
+  delay(500);
+}
+
+void showData()
+{
+  Serial.println("DATA RECEIVED:");
+  Serial.println(slave_data.connectionStatus);
+  Serial.println(slave_data.oilTemp);
+  Serial.println(slave_data.coolantTemp);
+  Serial.println(slave_data.MAF / 100.0);
+  Serial.println(slave_data.misfireCounter);
+  Serial.println(slave_data.batteryVoltage / 100.0);
+}
+
+void convertByteArrayToStructure()
+{
+}
