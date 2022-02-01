db0d512bb824753e0a9a0b1b33f9c8e04a1a3ed0
rinojk <>
2022-02-01 22:23:45 +0200

    working with needed data structure

--- BoardDiagnosticSystem/BoardDiagnosticSystem.ino
+++ BoardDiagnosticSystem/BoardDiagnosticSystem.ino
@@ -18,36 +18,47 @@
 #define i2c_slave_address 17
 
 // You can add more variables into the struct, but the default limit for transfer size in the Wire library is 32 bytes
-struct SLAVE_DATA {
-    uint16_t sensor;     // use specific declarations to ensure communication between 16bit and 32bit controllers
-    uint16_t oilTemp = 89;
+struct SLAVE_DATA
+{
+    int16_t sensor; // use specific declarations to ensure communication between 16bit and 32bit controllers
+    int16_t connectionStatus = 1;
+    int16_t oilTemp = 89;
+    int16_t coolantTemp = 90;
+    int16_t MAF = 91;
+    int16_t misfireCounter = 92;
+    int16_t batteryVoltage = 93;
 };
 
-struct SLAVE_CONFIG {
-    uint8_t val;        // use specific declarations to ensure communication between 16bit and 32bit controllers
+struct SLAVE_CONFIG
+{
+    uint8_t val; // use specific declarations to ensure communication between 16bit and 32bit controllers
 };
 
 SLAVE_DATA slave_data;
 SLAVE_CONFIG slave_config;
 
-void setup() {
-    Wire.begin(i2c_slave_address);    // i2c Slave address
-    Wire.onRequest(requestEvent);     // when the Master makes a request, run this function
-    Wire.onReceive (receiveEvent);    // when the Master sends us data, run this function
-    slave_config.val = 1;              // This is how much we increment after each request
+void setup()
+{
+    Wire.begin(i2c_slave_address); // i2c Slave address
+    Wire.onRequest(requestEvent);  // when the Master makes a request, run this function
+    Wire.onReceive(receiveEvent);  // when the Master sends us data, run this function
+    slave_config.val = 1;          // This is how much we increment after each request
 }
 
-void loop() {
-
+void loop()
+{
 }
 
-void requestEvent() {
+void requestEvent()
+{
     i2cSimpleWrite(slave_data);            // Send the Master the sensor data
-    slave_data.sensor += slave_config.val;  // Simulate updated sensor data
+    slave_data.sensor += slave_config.val; // Simulate updated sensor data
 }
 
-void receiveEvent (int payload) {
-    if ( payload == sizeof(slave_config) ){
-        i2cSimpleRead(slave_config);         // Receive new data from the Master
+void receiveEvent(int payload)
+{
+    if (payload == sizeof(slave_config))
+    {
+        i2cSimpleRead(slave_config); // Receive new data from the Master
     }
- }
+}
--- BoardInfoSystem/i2cSimpleTransfer.h
+++ BoardInfoSystem/i2cSimpleTransfer.h
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-template <typename T> unsigned int i2cSimpleWrite (const T& value) {
+template <typename T> int i2cSimpleWrite (const T& value) {
     Wire.write((byte *) &value, sizeof (value));
     return sizeof (value);
 }
 
-template <typename T> unsigned int i2cSimpleRead(T& value) {
+template <typename T> int i2cSimpleRead(T& value) {
     byte * p = (byte*) &value;
-    unsigned int i;
+    int i;
     for (i = 0; i < sizeof value; i++)
         *p++ = Wire.read();
     return i;
